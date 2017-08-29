1f09ceed12eedada50a369859f564f7841379566
my5t3ry <sascha.bast@dgm-kommunikation.de>
2017-08-29 10:40:35 +0200

    init

---
+++ src/main/java/de/my5t3ry/services/ProjectPathParser.java
@@ -0,0 +1,45 @@
+package de.my5t3ry.services;
+
+import de.my5t3ry.utils.GZipFile;
+import org.apache.commons.io.FileUtils;
+import org.json.JSONObject;
+import org.json.XML;
+import org.springframework.stereotype.Service;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * created by: sascha.bast
+ * since: 27.08.17
+ */
+@Service
+public class ProjectPathParser {
+    public void parsePath(final File dir) {
+        getAbletonProjectFiles(dir).forEach(file -> parseAbletonFile(file));
+
+    }
+
+    private List<File> getAbletonProjectFiles(final File dir) {
+
+        final String[] validExtension = {"als"};
+        return new ArrayList<>(FileUtils.listFiles(dir, validExtension, true));
+    }
+
+    private void parseAbletonFile(final File file) {
+        try {
+            final GZipFile gZipFile = new GZipFile(file);
+            final File outputFile = new File("/var/tmp/" + file.getName());
+            gZipFile.decompress(outputFile);
+            String xmlString = FileUtils.readFileToString(outputFile);
+            JSONObject xmlJSONObj = XML.toJSONObject(xmlString);
+            String jsonPrettyPrintString = xmlJSONObj.toString();
+            System.out.println(jsonPrettyPrintString);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
--- src/main/java/de/my5t3ry/view/ConfigurationController.java
+++ src/main/java/de/my5t3ry/view/ConfigurationController.java
@@ -1,10 +1,13 @@
 package de.my5t3ry.view;
 
+import de.my5t3ry.services.ProjectPathParser;
+import javafx.concurrent.Task;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.DirectoryChooser;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.io.File;
 
@@ -14,6 +17,9 @@ import java.io.File;
  */
 @org.springframework.stereotype.Controller
 public class ConfigurationController extends Controller {
+    @Autowired
+    private ProjectPathParser projectPathParser;
+
     @FXML
     AnchorPane configurationRoot;
 
@@ -27,7 +33,7 @@ public class ConfigurationController extends Controller {
 
     @Override
     public void init() {
-    
+
     }
 
 
@@ -40,11 +46,34 @@ public class ConfigurationController extends Controller {
     @FXML
     public void onSelectDataButton(ActionEvent event) {
         DirectoryChooser chooser = new DirectoryChooser();
-//        chooser.setInitialDirectory(Datastore.getInstance().getDataDir().toFile());
         File dir = chooser.showDialog(configurationRoot.getScene().getWindow());
+
+        ProgressForm pForm = new ProgressForm();
+
+        Task<Void> task = new Task<Void>() {
+            @Override
+            public Void call() throws InterruptedException {
+                updateProgress(10, 10);
+
+                projectPathParser.parsePath(dir);
+                return null;
+            }
+        };
+
+        pForm.activateProgressBar(task);
+
+        task.setOnSucceeded(successEvent -> {
+            pForm.getDialogStage().close();
+            dataButton.setDisable(false);
+        });
+
+        dataButton.setDisable(true);
+        pForm.getDialogStage().show();
+
+        Thread thread = new Thread(task);
+        thread.start();
         if (dir == null) {
             return;
         }
-//        Datastore.getInstance().setDataDir(Paths.get(dir.getAbsolutePath()));
     }
 }
